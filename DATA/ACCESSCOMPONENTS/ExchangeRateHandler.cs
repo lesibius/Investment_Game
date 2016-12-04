79df34439cece1383fe299d20b4fc7ccf9b7d73f
lesibius <clementin.castellano@gmail.com>
2016-12-04 15:33:21 +0100

    Created an exchange rate provider in the data layer

---
+++ DATA/ACCESSCOMPONENTS/ExchangeRateHandler.cs
@@ -0,0 +1,14 @@
+using System;
+using FinanceLib.ValueOperator;
+
+namespace Investment_Game.Data
+{
+    
+    public class ExchangeRateHandler : IExchangeRateProvider
+    {
+        
+    }
+
+
+
+}
