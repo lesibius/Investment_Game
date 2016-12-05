3fd59ad1cffddfe6ab00e9f62e65dd7ce17b299a
lesibius <clementin.castellano@gmail.com>
2016-12-05 08:29:07 +0100

    Removed the TestLibrary.sh file and put content in CompileUbuntu.sh (modified for library interaction with DATA layer)

--- BUSINESS/COMPONENTS/FINANCE/FinanceLib/TestLibrary.sh
+++
@@ -1,7 +0,0 @@
-ValueOperator=$(echo "ValueOperator/*.cs")
-Investment=$(echo "Investment/*.cs")
-Measurement=$(echo "Measurement/*.cs")
-TestFile=$(echo "Test/TestFile.cs")
-dll=$(echo "-r:System.Drawing.dll -r:System.Windows.Forms.DataVisualization.dll")
-
-mcs -out:Test/TestFile.exe $TestFile $ValueOperator $Investment $Measurement $dll
--- CompileUbuntu.sh
+++ CompileUbuntu.sh
@@ -1,16 +1,32 @@
 section_line=$(echo "#########################################################")
 justify=$(echo "               ")
 
+#################################################################################################
+#				Retrieving Files to Compile					#
+#################################################################################################
+
 echo $section_line
 echo "$justify Retrieving files to compile"
 echo $section_line
+
+#					DLL Files						#
+
 echo "Retrieving dll files"
 dll_files=$(echo "-r:/usr/lib/cli/MySql.Data-6.4/MySql.Data.dll -r:System.Data.dll")
+
+#					UI Files						#
+
 echo "Retrieving UI files"
 UI_Global=$(echo "PRESENTATION/INTERFACE/*.cs PRESENTATION/LOGIC/*.cs")
 UI_CMD=$(echo "PRESENTATION/UI/CMD/*.cs")
+
+#					BUSINESS Files						#
 echo "Retrieving business files"
 BUSINESS=$(echo "BUSINESS/COMPONENTS/DATAMANAGEMENT/*.cs BUSINESS/WORKFLOW/*.cs")
+
+echo "Retrieving FinanceLib"
+FINANCELIB=$(echo "BUSINESS/COMPONENTS/FINANCE/FinanceLib/ValueOperator/*.cs BUSINESS/COMPONENTS/FINANCE/FinanceLib/Investment/*.cs BUSINESS/COMPONENTS/FINANCE/FinanceLib/Measurement/*.cs")
+
 echo "Retrieving data files"
 DATA=$(echo "DATA/ACCESSCOMPONENTS/*.cs")
 echo "Retrieving interface files"
@@ -22,8 +38,10 @@ echo $section_line
 echo "$justify Compiling Applications"
 echo $section_line
 echo "Compiling command line application"
-dmcs ApplicationCMD.cs $UI_Global $UI_CMD $BUSINESS $DATA $INTERFACE $dll_files
-echo
+echo "Aborted"
+#dmcs ApplicationCMD.cs $UI_Global $UI_CMD $BUSINESS $FINANCELIB $DATA $INTERFACE $dll_files
+echo  "Compiling test application"
+dmcs BUSINESS/COMPONENTS/FINANCE/FinanceLib/Test/*.cs $UI_Global $UI_CMD $BUSINESS $FINANCELIB $DATA $INTERFACE $dll_files
 
 echo "Compilation: done"
 
